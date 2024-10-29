1b6c556cdd4f7b9b5572172958538d088e83a5f3
master <sy21977@gmail.com>
2024-10-29 19:38:56 +0900

    10/29 java spring study

---
+++ src/main/java/hello/hello_spring/controller/HelloController.java
@@ -0,0 +1,70 @@
+package hello.hello_spring.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+    @GetMapping("hello")
+    public String hello(Model model){
+        model.addAttribute("data", "spring!!");
+        return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(name="name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name){
+        return "hello "+name; // "hello spring"
+
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello{
+        private String name;
+
+        public String getName(){
+            return name;
+        }
+
+        public void setName(String name){
+            this.name=name;
+        }
+    }
+
+    @GetMapping("test-api")
+    @ResponseBody
+    public Test testApi(@RequestParam("name") String name){
+        Test api = new Test();
+        api.setMaple(name);
+        return api;
+    }
+
+    static class Test{
+        private String maple;
+
+        public String getMaple() {
+            return maple;
+        }
+
+        public void setMaple(String maple) {
+            this.maple = maple;
+        }
+    }
+
+
+}
