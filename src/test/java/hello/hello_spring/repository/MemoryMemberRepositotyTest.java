9dc9f49f3c8a15d30ce92c1098605fdcb34c3c26
master <sy21977@gmail.com>
2024-10-30 16:42:19 +0900

    10/30 java spring study

---
+++ src/test/java/hello/hello_spring/repository/MemoryMemberRepositotyTest.java
@@ -0,0 +1,23 @@
+package hello.hello_spring.repository;
+
+import hello.hello_spring.domain.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MemoryMemberRepositotyTest {
+
+    MemberRepository repositoty = new MemoryMemberRepository();
+
+    @Test
+    public void save(){
+        Member member = new Member();
+        member.setName("spring");
+
+        repositoty.save(member);
+
+        Member result = repositoty.findById(member.getId()).get();
+        //Assertions.assertEquals(result, member);
+        //Assertions.assertEquals(member, null);
+        Assertions.assertThat(member).isEqualTo(result);
+    }
+}
